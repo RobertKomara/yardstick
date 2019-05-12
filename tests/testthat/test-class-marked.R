b21115850f8d4fd2cbd3517229be2c2556f36a32
Robert Komara <rkomara@gmail.com>
2019-05-12 22:34:35 +0100

    Write tests for binary `marked` class copied over from `test-class-j_index.R`

---
+++ tests/testthat/test-class-marked.R
@@ -0,0 +1,45 @@
+context("Markedness")
+
+# ------------------------------------------------------------------------------
+
+lst <- data_altman()
+pathology <- lst$pathology
+path_tbl <- lst$path_tbl
+
+pred_ch <- quote(scan)
+
+test_that('Two class', {
+  expect_equal(
+    marked(pathology, truth = "pathology", estimate = "scan")[[".estimate"]],
+    (231/263) + (54/81)  - 1
+  )
+  expect_equal(
+    marked(path_tbl)[[".estimate"]],
+    (231/263) + (54/81)  - 1
+  )
+  expect_equal(
+    marked(pathology, pathology, scan)[[".estimate"]],
+    (231/263) + (54/81)  - 1
+  )
+})
+
+# ------------------------------------------------------------------------------
+
+# multi_ex <- data_three_by_three()
+# micro <- data_three_by_three_micro()
+#
+# test_that('Three class', {
+#
+#   expect_equal(
+#     j_index(multi_ex, estimator = "macro")[[".estimate"]],
+#     macro_metric(j_index_binary)
+#   )
+#   expect_equal(
+#     j_index(multi_ex, estimator = "macro_weighted")[[".estimate"]],
+#     macro_weighted_metric(j_index_binary)
+#   )
+#   expect_equal(
+#     j_index(multi_ex, estimator = "micro")[[".estimate"]],
+#     with(micro, sum(tp) / sum(p) + sum(tn) / sum(n) - 1)
+#   )
+# })
